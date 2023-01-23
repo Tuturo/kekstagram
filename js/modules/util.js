32a2aa0ef26aa27ccfce33a7e00faa599884fc9f
Tuturo <olenberg.vadim@gmail.com>
2023-01-23 18:29:47 +0300

    Добавляет функцию по поиску рандомного числа в диапазоне

--- js/modules/util.js
+++ js/modules/util.js
@@ -1,3 +1,18 @@
+// Найти рандомное число из диапазона включительно
+
+const getRandomInt = (min, max) => {
+    if (min < 0 || max < 0) {
+        return -1;
+    }
+
+    if (min > max) {
+        [min, max] = [max, min];
+    }
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
 // Функция для проверки максимальной длины строки
 
 function checkStringLength(currentString, maxLength) {
